27a19184b09638c49b58608760492aa908bdfb8d
Aridane Garcia <aridane.garcia@proton.me>
2026-02-22 19:34:55 +0000

    style: header glassmorphism con backdrop-blur y texto blanco

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,14 @@
 export function Header() {
     return (
-        <header className="fixed top-0 w-full bg-white shadow-md z-50">
+        <header className="fixed top-0 w-full bg-transparent backdrop-blur-md  shadow-lg z-50">
            <nav className="max-w-7xl mx-auto px-4 py-8 flex justify-between items-center">
-            <span className="text-2xl font-bold text-gray-800">AriDev</span>
+            <span className="text-2xl font-bold text-cyan-400">AriDev</span>
             
             <ul className="flex gap-6">
-                <li><a href="#inicio" className="hover:text-blue-600">Inicio</a></li>
-                <li><a href="#proyectos" className="hover:text-blue-600">Proyectos</a></li>
-                <li><a href="#sobre-mí" className="hover:text-blue-600">Sobre mí</a></li>
-                <li><a href="#contacto" className="hover:text-blue-600">Contacto</a></li>
+                <li><a href="#inicio" className="text-white hover:text-cyan-400">Inicio</a></li>
+                <li><a href="#proyectos" className="text-white hover:text-cyan-400">Proyectos</a></li>
+                <li><a href="#sobre-mí" className="text-white hover:text-cyan-400">Sobre mí</a></li>
+                <li><a href="#contacto" className="text-white hover:text-cyan-400">Contacto</a></li>
             </ul>
            </nav>
         </header>
